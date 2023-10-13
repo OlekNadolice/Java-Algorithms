bab92aae6b7349f60184e47f7464826187f6a91e
OlekNadolice <nadolice1997@wp.pl>
2023-10-13 14:18:50 +0200

    ADD-CAN-BALANCE-EXERCISE

---
+++ src/main/java/org/example/tasks/CanBalance.java
@@ -0,0 +1,47 @@
+package org.example.tasks;
+
+
+import static java.lang.Math.floor;
+
+public class CanBalance {
+ /*   Given a non-empty array,
+ return true if there is a place to split the array so
+  that the sum of the numbers on one side is equal to
+  the sum of the numbers on the other side.
+
+    canBalance([1, 1, 1, 2, 1]) → true
+    canBalance([2, 1, 1, 2, 1]) → false
+    canBalance([10, 10]) → true
+
+  */
+
+
+    public static boolean countBalance(int[] arr) {
+        int middleIndex = arr.length / 2;
+        int leftSum = 0;
+        int rightSum = 0;
+
+        for (int a = 0; a <= arr.length - 1; a++) {
+            if (a == middleIndex) continue;
+
+            if (a < middleIndex) {
+                leftSum += arr[a];
+            } else {
+                rightSum += arr[a];
+            }
+
+
+        }
+        ;
+
+        if (leftSum + arr[middleIndex] == rightSum) {
+            return true;
+        }
+
+        return rightSum + arr[middleIndex] == leftSum;
+
+
+    }
+
+
+}
---
+++ src/test/java/org/example/tasks/CanBalanceTest.java
@@ -0,0 +1,18 @@
+package org.example.tasks;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CanBalanceTest {
+    @Test
+   public void countBalance() {
+        var data1 = new int[] {1,1,1,2,1};
+        var data2 = new int[] {2,1,1,2,1};
+        var data3 = new int[] {10,10};
+
+        assertTrue(CanBalance.countBalance(data1));
+        assertFalse(CanBalance.countBalance(data2));
+        assertTrue(CanBalance.countBalance(data3));
+    }
+}
