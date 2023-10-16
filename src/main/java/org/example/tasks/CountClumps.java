d741850fbc20313c78fe4ce7b939d7f3e56c4b48
OlekNadolice <nadolice1997@wp.pl>
2023-10-16 17:38:36 +0200

    CountClumps

---
+++ src/main/java/org/example/tasks/CountClumps.java
@@ -0,0 +1,52 @@
+package org.example.tasks;
+
+public class CountClumps {
+
+/*   Say that a "clump" in an array is a series of 2 or more adjacent elements of the same value.
+ Return the number of clumps in the given array.
+
+   countClumps([1, 2, 2, 3, 4, 4]) → 2
+    countClumps([1, 1, 2, 1, 1]) → 2
+    countClumps([1, 1, 1, 1, 1]) → 1
+
+*/
+
+
+    public static int countClumps(int[] arr) {
+        int count = 0;
+        if (arr.length == 0) return count;
+
+
+        for (int a = 0; a <= arr.length - 2; ) {
+            if (arr[a] != arr[a + 1]) {
+                a = a + 1;
+            } else {
+                count = count + 1;
+                int indexToStart = a + 1;
+                boolean shouldAssignIndex = true;
+                for(int b = a + 1; b <= arr.length - 1 ; b++) {
+                    if(arr[a] != arr[b]) {
+                        a = b;
+                        shouldAssignIndex = false;
+                        break;
+                    } else {
+                        indexToStart = b;
+                    }
+                }
+
+                if(shouldAssignIndex) {
+                    a = indexToStart;
+                }
+
+
+
+            }
+
+        }
+
+
+        return count;
+    }
+
+
+}
---
+++ src/test/java/org/example/tasks/CountClumpsTest.java
@@ -0,0 +1,22 @@
+package org.example.tasks;
+
+import org.junit.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CountClumpsTest {
+
+    @Test
+  public  void countClumps() {
+
+        var data1 = new int[] {1,2,2,3,4,4};
+        var data2 = new int[] {1,1,2,1,1};
+        var data3 = new int[] {1,1,1,1,1};
+
+
+        assertEquals(2, CountClumps.countClumps(data1));
+        assertEquals(2, CountClumps.countClumps(data2));
+        assertEquals(1, CountClumps.countClumps(data3));
+    }
+}
